5180a5e4a925f97333391ed19845d5745fd3a21e
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-02 00:27:03 +0200

    render the information of post in show page

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/post/create', 'PostsController@create');
 Route::get('/post/{post}', 'PostsController@show');
+Route::get('/post/create', 'PostsController@create');
 Route::post('/post', 'PostsController@store');
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
