b722488d05585ae207d2ad794cc5eb077d9c23f4
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-01 22:12:42 +0200

    save the data of post form

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,18 +6,30 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
     }
 
-    public function store(Request $request)
+    public function store()
     {
-        $data = $request->validate([
+        $data = request()->validate([
             'caption' => 'required',
             'image' => ['required', 'image']
         ]);
 
-        dd(request()->all());
+        $imagePath = request('image')->store('uploads', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
